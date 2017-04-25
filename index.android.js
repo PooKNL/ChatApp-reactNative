700f41bde1de2bf2a584cd95cb3bbcacaea00c3c
Robert Jan Hogerbrugge <rj.hogerbrugge@hotmail.com>
2017-04-25 14:44:53 +0200

    setup

---
+++ index.android.js
@@ -0,0 +1,9 @@
+/**
+ * Android Entry Point for ShatApp
+ * @flow
+ */
+
+import { AppRegistry } from 'react-native';
+import ShatApp from './ShatApp';
+
+AppRegistry.registerComponent('ShatApp', () => ShatApp);
